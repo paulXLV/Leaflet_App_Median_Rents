2964550055ecd0330a31105310cdbf2c1b7761f9
paulXLV <paul@excelvertical.com>
2018-12-16 23:31:15 -0500

    did Heroku

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app = Flask(__name__)
 # Use flask_pymongo to set up mongo connection
 # conn = "mongodb://localhost:27017/"
 # client = pymongo.MongoClient(conn)
-db = "mongodb://<dbuser>:<300atrium>@ds155097.mlab.com:55097/heroku_7t9dkd05"
+db = "mongodb://dbuser:300atrium@ds155097.mlab.com:55097/heroku_7t9dkd05"
 # db = client.zipDB
 #db.zipDB.drop()
 all_data = "Yearly_Top3_Tech_Loc_ZRI_Hist.csv"
