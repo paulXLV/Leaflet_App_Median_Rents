e2c064b1b23429ea08f361a4738d4d4c35ea8d50
paulXLV <paul@excelvertical.com>
2018-12-18 12:31:40 -0500

    mongo db

--- app.py
+++ app.py
@@ -7,24 +7,17 @@ import pandas as pd
 import os
 import csv
 from bson.json_util import dumps
-#print(MONGO_URI)
-#print(uri)
-uri_key = os.environ.get("MONGO_URL")
-app = Flask(__name__)
-#app.config['MONGO_DBNAME'] = 
-app.config['MONGO_URI'] = uri_key
-db = PyMongo(app)
 
 # create instance of Flask app
 
-#app = Flask(__name__)
+app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
 # conn = "mongodb://localhost:27017/"
-# client = pymongo.MongoClient(conn)
-#app.config['MONGO_URI']
+client = pymongo.MongoClient("mongodb://heroku_7t9dkd05:kt8li5obnhhria0qf5vvtbh1cm@ds155097.mlab.com:55097/heroku_7t9dkd05")
+
 #"mongodb://heroku_7t9dkd05:kt8li5obnhhria0qf5vvtbh1cm@ds155097.mlab.com:55097/heroku_7t9dkd05/"
-# db = client.zipDB
+db = client["heroku_7t9dkd05"]
 #db.zipDB.drop()
 all_data = "Yearly_Top3_Tech_Loc_ZRI_Hist.csv"
 all_data_pd = pd.read_csv(all_data)
@@ -35,7 +28,8 @@ db.zipColl.insert_many(zipData)
 
 @app.route("/")
 def home():
-    return render_template("heatmap.html")
+    return "ENTER /YEAR"
+    #return render_template("heatmap.html")
     
 @app.route("/<year>")
 def samples(year):
