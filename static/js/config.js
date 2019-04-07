6bbfd50a39b0dca9e675ab80617bf21deb189f92
paulXLV <paul@excelvertical.com>
2019-04-06 21:38:28 -0400

    update api access token

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1Ijoia3VsaW5pIiwiYSI6ImNpeWN6bjJ0NjAwcGYzMnJzOWdoNXNqbnEifQ.jEzGgLAwQnZCv9rA6UTfxQ";
+const API_KEY = "pk.eyJ1IjoieGx2ZXJ0aWNhbCIsImEiOiJjanU2OTlwZnUxaGFvNGRuemZtc3VmNGN5In0.w0lJPHXhAFCscMqcteQMaQ";
